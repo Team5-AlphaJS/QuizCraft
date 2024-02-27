3ed6e69d0b8664e482dc7913e4da1dfc85139f00
Martin Valov <maritn.valov98@gmail.com>
2024-02-27 23:25:43 +0200

    setting fribase

---
+++ quiz-craft/src/config/firebase-config.js
@@ -0,0 +1,21 @@
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbf4JulBxAjQzK4Mz_GnHE_4D7UUdzxfQ",
+  authDomain: "quiz-craft-3a9a5.firebaseapp.com",
+  projectId: "quiz-craft-3a9a5",
+  storageBucket: "quiz-craft-3a9a5.appspot.com",
+  messagingSenderId: "1056639557310",
+  appId: "1:1056639557310:web:c9d2482437afe8d55c88ef",
+  dataBaseURL: "https://quiz-craft-3a9a5-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// const app = initializeApp(firebaseConfig);
+
+export const app = initializeApp(firebaseConfig);
+// the Firebase authentication handler
+export const auth = getAuth(app);
+// the Realtime Database handler
+export const db = getDatabase(app);
