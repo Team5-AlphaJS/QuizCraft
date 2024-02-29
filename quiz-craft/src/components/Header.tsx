ecaa17b41203d88ea83f6a53a60a73ba1e4513ba
Stefan Trajkovski <121553629+nobalancemaybe@users.noreply.github.com>
2024-02-29 17:53:12 +0100

    added login form

--- quiz-craft/src/components/Header.tsx
+++ quiz-craft/src/components/Header.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { ModeToggle } from './mode-toggle'
-import { Input } from "@/components/ui/input"
-import { Button } from './ui/button'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ModeToggle } from './mode-toggle';
+import { Input } from "@/components/ui/input";
+import { Button } from './ui/button';
+import { LoginForm } from './ui/login-form';
+
 const NavBar = () => {
+  const [showLogInForm, setShowLogInForm] = useState(false);
+
+  const toggleLoginForm = () => {
+    setShowLogInForm(prev => !prev);
+  };
+
+  const hideLoginForm = () => {
+    setShowLogInForm(false);
+  };
+
   return (
     <>
       <div className="flex justify-between items-center">
-        <div className="flex justify-between items-center ">
+        <div className="flex justify-between items-center">
           <Link to="/" className="px-8">
             Quiz Craft
           </Link>
@@ -15,17 +27,26 @@ const NavBar = () => {
             Random free quiz
           </Button>
         </div>
-        <div >
+        <div>
           <Input placeholder="Search quizzes" />
         </div>
         <ModeToggle />
-        <Link to="/login"  className="px-8">
+        <Button onClick={toggleLoginForm} className="px-8">
           Login
-        </Link>
+        </Button>
       </div>
+      {showLogInForm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="p-8 rounded-lg w-96 max-w-full flex flex-col items-center">
+            <LoginForm />
+            <Button onClick={hideLoginForm} className="mt-4">
+              Back
+            </Button>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default NavBar
-
+export default NavBar;
--- quiz-craft/src/pages/LandingPage.tsx
+++ quiz-craft/src/pages/LandingPage.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import CategoriesNav from '@/components/ui/categories-nav';
+
+
 const LandingPage: React.FC = () => {
+
   return (
     <>
       <div className="container mx-auto py-8">
