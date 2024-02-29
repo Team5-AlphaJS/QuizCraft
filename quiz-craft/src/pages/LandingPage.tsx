9513737188b192901eb08cf07ffd00b06fa1f628
Stefan Trajkovski <121553629+nobalancemaybe@users.noreply.github.com>
2024-02-29 17:10:07 +0100

    CategoriesNav component added

--- quiz-craft/src/pages/LandingPage.tsx
+++ quiz-craft/src/pages/LandingPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-
+import CategoriesNav from '@/components/ui/categories-nav';
 const LandingPage: React.FC = () => {
   return (
     <>
       <div className="container mx-auto py-8">
         <Header />
       </div>
-      <div>Categories</div>
+      <CategoriesNav />
       <div>Quizzes</div>
       <Footer />
     </>
